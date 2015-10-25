05d42509b4a8e83252d4e906ffa5dbbcc84d7b7d
Junichi Uekawa <dancer@netfort.gr.jp>
2015-10-26 07:21:16 +0900

    add an integration test. Some unit tests fail.

---
+++ integrationtest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+cleanup() {
+    fusermount -u mountpoint2 || true
+    fusermount -u mountpoint || true
+    rm -rf tmp
+}
+cleanup
+mkdir mountpoint2 tmp || true
+./configure.js
+ninja
+./out/gitlstree mountpoint/
+unionfs-fuse tmp=RW:mountpoint mountpoint2
+
+# Start of building the first copy.
+cd mountpoint2
+mkdir out
+./configure.js
+ninja || true
+ninja out/hello_world
+./out/hello_world
+cd ..
+
+cleanup
