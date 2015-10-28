5c369e236530e1a86ce51a4634fede67b7ba10f5
Junichi Uekawa <dancer@netfort.gr.jp>
2015-10-28 14:50:30 +0900

    add include guard.

--- basename.h
+++ basename.h
@@ -1,7 +1,11 @@
-#include<string>
+#ifndef __BASENAME_H__
+#define __BASENAME_H__
+#include <string>
 
 // After the last /.
 const std::string BaseName(const std::string n);
 
 // Up to and excluding the last /.
 const std::string DirName(const std::string n);
+
+#endif
