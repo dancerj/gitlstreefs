5b9bd2ea8d7c52f04970d7a6f20463cf8ed540b6
Junichi Uekawa <dancer@netfort.gr.jp>
2015-11-09 10:56:09 +0900

    make sure cache is used.

--- cached_file_test.cc
+++ cached_file_test.cc
@@ -11,7 +11,10 @@ static char kTestString[] = "HogeFuga";
 int main(int argc, char** argv) {
   Cache c(".cache/");
   const Cache::Memory* m = c.get("test1", []() -> string { return string(kTestString); });
+  const Cache::Memory* m2 = c.get("test1", []() -> string { return "fail"; });
   string test(reinterpret_cast<const char*>(m->memory()), m->size());
+  assert(test == kTestString);
+  string test2(reinterpret_cast<const char*>(m2->memory()), m2->size());
   assert(test == kTestString);
   return 0;
 }
