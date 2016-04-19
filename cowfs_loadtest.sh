06a021b0be90606a5bde589de9cd599efcbdd4f2
Junichi Uekawa <dancer@netfort.gr.jp>
2016-04-19 10:46:09 +0900

    Initial try into scripting the load test.

---
+++ cowfs_loadtest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+TESTDIR=out/cowfsloadtmp
+cleanup() {
+    fusermount -z -u $TESTDIR/workdir || true
+}
+cleanup
+trap cleanup exit
+
+rm -rf $TESTDIR/{shadow,workdir,repo}
+mkdir -p $TESTDIR/{shadow,workdir,repo}
+
+# start file system
+out/cowfs $TESTDIR/workdir -o nonempty \
+	  --lock_path=$TESTDIR/lock \
+	  --underlying_path=$TESTDIR/shadow \
+	  --repository=$TESTDIR/repo \
+	  -d
+sleep 1
+
+cd $TESTDIR/workdir
+../../experimental/parallel_writer
