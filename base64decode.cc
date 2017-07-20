2a1ecff6cc03dc166a4e86f8cf210c8544f9a591
Junichi Uekawa <dancer@netfort.gr.jp>
2017-07-20 19:09:27 +0900

    move table initialization to be static at load time.
    This slows down load but makes everything faster.
    
    It is an excercise for the reader to make the table statically generated.

--- base64decode.cc
+++ base64decode.cc
@@ -3,42 +3,56 @@
 #include <assert.h>
 
 #include <string>
-#include <vector>
+#include <array>
 
 using std::string;
-using std::vector;
+using std::array;
 
+namespace {
 static constexpr char kAlphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-constexpr int64_t kEmptyChar = -1;
-
-string base64decode(const string& b64) {
-  string output;
-  output.reserve(b64.size() * 0.7);
-  vector<int64_t> lookup(256);
-  size_t i;
-  for (auto& v: lookup) { v = kEmptyChar; }
-  for (i = 0; i < sizeof(kAlphabet); ++i) {
-    lookup[kAlphabet[i]] = i;
+static constexpr int64_t kEmptyChar = -1;
+class Base64Decoder {
+public:
+  Base64Decoder() {
+    for (auto& v: lookup_) { v = kEmptyChar; }
+    for (size_t i = 0; i < sizeof(kAlphabet); ++i) {
+      lookup_[kAlphabet[i]] = i;
+    }
   }
+  ~Base64Decoder() {}
 
-  for (size_t position = 0; position < b64.size(); ) {
-    uint64_t result = 0;
-    int j;
-    for (j = 0; j < 4 && position < b64.size(); ++j) {
-      int64_t t = 0;
-      do {
-	t = lookup[b64[position++]];
-      } while (t == kEmptyChar && position < b64.size());
-      if (t != kEmptyChar) {
-	result |= (t << ((3 - j) * 6));
-      } else {
-	break;
+  string doit(const string& b64) {
+    string output;
+    output.reserve(b64.size() * 0.7);
+
+    for (size_t position = 0; position < b64.size(); ) {
+      uint64_t result = 0;
+      int j;
+      for (j = 0; j < 4 && position < b64.size(); ++j) {
+	int64_t t = 0;
+	do {
+	  t = lookup_[b64[position++]];
+	} while (t == kEmptyChar && position < b64.size());
+	if (t != kEmptyChar) {
+	  result |= (t << ((3 - j) * 6));
+	} else {
+	  break;
+	}
+      }
+      for (int k = 0; k < j - 1; ++k) {
+	char c = char((result >> ((2 - k) * 8)) & 255);
+	output += c;
       }
     }
-    for (int k = 0; k < j - 1; ++k) {
-      char c = char((result >> ((2 - k) * 8)) & 255);
-      output += c;
-    }
+    return output;
   }
-  return output;
+private:
+  array<int64_t, 256> lookup_{};
+};
+
+static Base64Decoder b;
+
+}
+string base64decode(const string& b64) {
+  return b.doit(b64);
 }
