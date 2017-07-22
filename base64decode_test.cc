7d8580860bb2c1650984548852021fc2f1188e4a
Junichi Uekawa <dancer@netfort.gr.jp>
2017-07-22 16:19:44 +0900

    Try using a postfix notation just because it's possible.
    Not sure if this makes things clearer.
    
    Use R"()" notation on the way so that source code is more visible.

--- base64decode_test.cc
+++ base64decode_test.cc
@@ -2,18 +2,31 @@
 
 #include "base64decode.h"
 
+std::string operator"" _b64(const char* str, std::size_t len) {
+  std::string s(str, len);
+  return base64decode(s);
+}
+
 int main(int argc, char** argv) {
-  assert(base64decode("aGVsbG8gd29ybGQK") == "hello world\n");
-  assert(base64decode("Mg==") == "2");
-  assert(base64decode("@#$%") == "");
+  assert("aGVsbG8gd29ybGQK"_b64 == "hello world\n");
+  assert("Mg=="_b64 == "2");
+  assert("@#$%"_b64 == "");
 
   // Ignore whitespace characters.
-  assert(base64decode("aGVsbG8gd29ybGQK\n") == "hello world\n");
-  assert(base64decode("I2lmICFkZWZpbmVkKFNUUlVUSUxfSF9fKQojZGVmaW5lIFNUUlVUSUxfSF9f\nCiNpbmNsdWRlIDxzdHJpbmc+CnN0ZDo6c3RyaW5nIFJlYWRGcm9tRmlsZU9y\nRGllKGNvbnN0IHN0ZDo6c3RyaW5nJiBmaWxlbmFtZSk7CnN0ZDo6c3RyaW5n\nIFBvcGVuQW5kUmVhZE9yRGllKGNvbnN0IHN0ZDo6c3RyaW5nJiBjb21tYW5k\nLAoJCQkgICAgICBpbnQqIG1heWJlX2V4aXRfY29kZSA9IG51bGxwdHIpOwoj\nZW5kaWYK\n") == "#if !defined(STRUTIL_H__)\n#define STRUTIL_H__\n#include <string>\nstd::string ReadFromFileOrDie(const std::string& filename);\nstd::string PopenAndReadOrDie(const std::string& command,\n			      int* maybe_exit_code = nullptr);\n#endif\n");
+  assert("aGVsbG8gd29ybGQK\n"_b64 == "hello world\n");
+  assert("I2lmICFkZWZpbmVkKFNUUlVUSUxfSF9fKQojZGVmaW5lIFNUUlVUSUxfSF9f\nCiNpbmNsdWRlIDxzdHJpbmc+CnN0ZDo6c3RyaW5nIFJlYWRGcm9tRmlsZU9y\nRGllKGNvbnN0IHN0ZDo6c3RyaW5nJiBmaWxlbmFtZSk7CnN0ZDo6c3RyaW5n\nIFBvcGVuQW5kUmVhZE9yRGllKGNvbnN0IHN0ZDo6c3RyaW5nJiBjb21tYW5k\nLAoJCQkgICAgICBpbnQqIG1heWJlX2V4aXRfY29kZSA9IG51bGxwdHIpOwoj\nZW5kaWYK\n"_b64 ==
+R"(#if !defined(STRUTIL_H__)
+#define STRUTIL_H__
+#include <string>
+std::string ReadFromFileOrDie(const std::string& filename);
+std::string PopenAndReadOrDie(const std::string& command,
+			      int* maybe_exit_code = nullptr);
+#endif
+)");
 
   // Make sure === works.
-  assert(base64decode("A===\n").size() == 0);
-  assert(base64decode("AA==\n").size() == 1);
-  assert(base64decode("AAA=\n").size() == 2);
-  assert(base64decode("AAAA\n").size() == 3);
+  assert("A===\n"_b64.size() == 0);
+  assert("AA==\n"_b64.size() == 1);
+  assert("AAA=\n"_b64.size() == 2);
+  assert("AAAA\n"_b64.size() == 3);
 }
