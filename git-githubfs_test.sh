d19c01fcc2a134720f6975df5aa549d013ddb86a
Junichi Uekawa <dancer@netfort.gr.jp>
2019-01-04 16:53:59 +0900

    Refactor some tests.
    Make trap better.

--- git-githubfs_test.sh
+++ git-githubfs_test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Sends requests to github. Should probably not be a continuous test.
+cleanup() {
+    fusermount -u mountpoint || true
+}
+cleanup
+
+trap cleanup exit
+./configure.js
+ninja
 ./out/git-githubfs --user=dancerj --project=gitlstreefs mountpoint/
 ls -l mountpoint
 grep 'git' mountpoint/README.md
 grep 'For testing symlink operation' mountpoint/testdata/symlink
-fusermount -u mountpoint
+echo '*** COMPLETE ***'
