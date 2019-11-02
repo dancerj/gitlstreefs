c532b23bd1aa8ad37e5d4fd4c2a51b48073895f9
Junichi Uekawa <dancer@netfort.gr.jp>
2019-11-02 20:06:04 +0900

    Compare thread and async.
    They seem to behave similar and slightly better behavior with async.

--- concurrency_limit_test.cc
+++ concurrency_limit_test.cc
@@ -1,28 +1,74 @@
 #include "concurrency_limit.h"
 
+#include <cassert>
+#include <future>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
 #include <unistd.h>
 #include <vector>
 
+using std::async;
 using std::cout;
 using std::endl;
+using std::future;
+using std::lock_guard;
+using std::mutex;
 using std::string;
 using std::thread;
 using std::vector;
 
-int main(int argc, char** argv) {
+void TestThread() {
   vector<thread> jobs;
+  int counter = 0;
+  std::mutex m{};
+
   // 20 iterations should finish in about 3 beats
   for (int i = 0; i < 20; ++i) {
-    jobs.emplace_back(thread([i]{
+    jobs.emplace_back(thread([i, &m, &counter]{
 	  char buffer[20];
 	  sprintf(buffer, "task %i", i);
 	  ScopedConcurrencyLimit l(buffer);
 	  // sleep for a quaver
 	  usleep(250000);
+	  {
+	    lock_guard<mutex> l(m);
+	    counter++;
+	  }
 	}));
   }
   for (auto& job : jobs) { job.join(); }
+  assert(counter == 20);
+}
+
+void TestAsync() {
+int counter = 0;
+  std::mutex m{};
+  {
+    vector<future<void> > jobs;
+
+    // 20 iterations should finish in about 3 beats
+    for (int i = 0; i < 20; ++i) {
+      jobs.emplace_back(async([i, &m, &counter]{
+	    char buffer[20];
+	    sprintf(buffer, "task %i", i);
+	    ScopedConcurrencyLimit l(buffer);
+	    // sleep for a quaver
+	    usleep(250000);
+	    {
+	      lock_guard<mutex> l(m);
+	      counter++;
+	    }
+	  }));
+    }
+  }
+
+  assert(counter == 20);
+}
+
+
+int main(int argc, char** argv) {
+  TestThread();
+  TestAsync();
 }
