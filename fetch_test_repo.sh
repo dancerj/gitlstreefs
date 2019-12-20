9725b71a70b1418c99005453957cb6ee658a11d9
Junichi Uekawa <dancer@netfort.gr.jp>
2019-12-20 09:06:31 +0900

    Clean up before creating the test repo

--- fetch_test_repo.sh
+++ fetch_test_repo.sh
@@ -2,8 +2,7 @@
 # prepare test repository
 set -ex
 
-if ! [ -d out/fetch_test_repo/ ]; then
-    mkdir -p out/fetch_test_repo/
-    cd out/fetch_test_repo/
-    git clone https://github.com/dancerj/gitlstreefs.git
-fi
+rm -rf out/fetch_test_repo/
+mkdir -p out/fetch_test_repo/
+cd out/fetch_test_repo/
+git clone https://github.com/dancerj/gitlstreefs.git
